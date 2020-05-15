259636f9f3961ff788792f782111c8a1a1ad6858
andrew <kerovap@mail.ru>
2020-05-15 17:21:53 +0700

    books= Book.object.all()

--- demo/models.py
+++ demo/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title=models.CharField(max_length=40)
+    title=models.CharField(max_length=40 ,blank=False ,unique=True)
+    description=models.TextField(max_length=500 ,blank=True)
+    price=models.DecimalField(default=0 ,max_digits=15, decimal_places=2)
+    published=models.DateField(default="1999-01-01")
+    is_published=models.BooleanField(default=False)
+    cover=models.ImageField(upload_to='covers/',default = "1482881197192578721.jpg")
--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
 # Create your views here.
+def first(request):
+    return HttpResponse("Hello World!")
+
+class Second(View):
+    books=Book.objects.all()
+    output=''
+
+    for book in books:
+        output+= (f'''We have {book.title} 
+        with ID {book.id} in our Database <br>''')
+
+    def get(self,request):
+        return HttpResponse(self.output)
